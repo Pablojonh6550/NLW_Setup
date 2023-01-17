4e8d510f65264342bb1519909a13a906d236143d
Pablojonh6550 <pablojonh6550@gmail.com>
2023-01-16 21:47:52 -0300

    feat: server create

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+
+const prisma = new PrismaClient();
+
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "beber",
+      },
+    },
+  });
+
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running!");
+  });
