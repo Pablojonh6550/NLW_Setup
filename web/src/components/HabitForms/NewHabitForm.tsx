863d4bc461a855452e41db8d115399a351334800
Pablojonh6550 <pablojonh6550@gmail.com>
2023-01-26 21:59:58 -0300

    feat: finalização do form

--- web/src/components/HabitForms/NewHabitForm.tsx
+++ web/src/components/HabitForms/NewHabitForm.tsx
@@ -1,8 +1,38 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+import { FormEvent, useState } from "react";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function NewHabitForms() {
+  const [title, setTitle] = useState("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+  }
+
+  function handleToggleWeekDays(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemoveOne = weekDays.filter((day) => day !== weekDay);
+      setWeekDays(weekDaysWithRemoveOne);
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay];
+
+      setWeekDays(weekDaysWithAddedOne);
+    }
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -13,11 +43,30 @@ export function NewHabitForms() {
         id="title"
         placeholder="ex.: Exercicios, dormir bem, etc..."
         autoFocus
+        onChange={(event) => setTitle(event.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="mt-3 flex flex-col gap-2">
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox.Root
+            key={`${weekDay}-${index}`}
+            className="flex items-center gap-3 group"
+            onCheckedChange={() => {
+              handleToggleWeekDays(index);
+            }}>
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-600 group-data-[state=checked]:border-green-600">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+            <span className="text-white leading-tight">{weekDay}</span>
+          </Checkbox.Root>
+        ))}
+      </div>
 
       <button
         type="submit"
--- web/src/components/summary/SummaryTable.tsx
+++ web/src/components/summary/SummaryTable.tsx
@@ -22,10 +22,9 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => {
-          return <HabitDay completed={3} amount={4} key={date.toString()} />;
-        })}
-
+        {summaryDates.map((date) => (
+          <HabitDay completed={3} amount={4} key={date.toString()} />
+        ))}
         {amountOfDaysToFill > 0 &&
           Array.from({ length: amountOfDaysToFill }).map(() => {
             return (
